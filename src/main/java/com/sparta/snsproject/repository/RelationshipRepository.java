b128c58ef55e0f8fa7e3a08505762a78fb4e36db
wlsql852 <wlsql852@naver.com>
2024-09-03 16:51:26 +0900

    친구 요청한 목록, 요청받은 목록 조회 추가

--- src/main/java/com/sparta/snsproject/repository/RelationshipRepository.java
+++ src/main/java/com/sparta/snsproject/repository/RelationshipRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.snsproject.repository;
 
 
+import com.sparta.snsproject.entity.AskStatus;
 import com.sparta.snsproject.entity.Relationship;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,5 +13,7 @@ import java.util.Optional;
 public interface RelationshipRepository extends JpaRepository<Relationship, Long> {
     Optional<Relationship> findByAskingIdAndAskedId(Long askingId, Long askedId);
 
-    List<Relationship> findAllByAskedId(Long askedId);
+    List<Relationship> findAllByAskedIdAndStatus(Long askedId, AskStatus status);
+
+    List<Relationship> findAllByAskingIdAndStatus(Long askingId, AskStatus status);
 }
