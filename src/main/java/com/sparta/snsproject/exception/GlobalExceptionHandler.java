ef501c9da95a30a2bc221950b5d574b2116943ab
dhPark <pdh6113@naver.com>
2024-09-04 20:05:37 +0900

    refactor:예외처리 ì 추가

--- src/main/java/com/sparta/snsproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/snsproject/exception/GlobalExceptionHandler.java
@@ -1,11 +1,17 @@
 package com.sparta.snsproject.exception;
 
+import com.mysql.cj.exceptions.WrongArgumentException;
 import com.sparta.snsproject.dto.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
 
 
@@ -30,5 +36,29 @@ public class GlobalExceptionHandler {
     public ApiResponse<?> handleExistFrandsNameException(ExistFrandsName e) {
         return ApiResponse.createError(e.getMessage(), HttpStatus.BAD_REQUEST.value());
     }
+    @ExceptionHandler(NoSignedUserException.class)
+    public ApiResponse<?> handleNoSignedUserException(NoSignedUserException e) {
+        return ApiResponse.createError(e.getMessage(), HttpStatus.NOT_FOUND.value());
+    }
+    @ExceptionHandler(WrongPasswordException.class)
+    public ApiResponse<?> handleWrongPasswordException(WrongPasswordException e) {
+        return ApiResponse.createError(e.getMessage(), HttpStatus.BAD_REQUEST.value());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        List<String> errors = new ArrayList<>();
+        e.getBindingResult().getAllErrors().forEach(error ->
+                errors.add(error.getDefaultMessage())
+        );
+        return getErrorResponse(status, String.join(",", errors));
+    }
+    //오류 메세지 내용
+    public ResponseEntity<Map<String, Object>> getErrorResponse(HttpStatus status, String message) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("status", status.name());
+        errorResponse.put("code", status.value());
+        errorResponse.put("message", message);
+        return new ResponseEntity<>(errorResponse, status);
+    }
 }
-
